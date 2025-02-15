a35e667039bf8c955a5f6eb427cbd0d627fda2da
Yajus Gakhar <yajus19csu349@ncuindia.edu>
2025-02-15 11:40:45 +0530

    Add message delete
    Sus messages can now be deleted the moment they're spotted

--- virustotal_discord_bot.py
+++ virustotal_discord_bot.py
@@ -18,8 +18,15 @@ async def send_message(message: Message, user_message: str) -> None:
             1]  # if the message is private, then send the message in the dms rather than in the group.
     try:
         response: str = get_response(user_message)
-        if response != '':
+        if response:
             await message.author.send(response) if is_private else await message.channel.send(response)
+
+            # Uncomment this if you wish for the bot to delete the sus link after detection. You can add a delay like this:
+            # await message.delete(delay=x) where x is the number of seconds for the delay
+
+            # await message.channel.send("The sus link will now be deleted. Do NOT click on the link.")
+            # await message.delete()
+            # TODO: save the sus URLs in a database (but why)
 
     except Exception as e:
         print(e)
@@ -32,7 +39,7 @@ async def send_message_edited(message: Message, user_message: str) -> None:
         user_message = user_message[1]
     try:
         response: str = get_edited_response(user_message)
-        if response != '':
+        if response:
             await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
@@ -50,6 +57,7 @@ async def on_message(message: Message) -> None:
     username: str = str(message.author)
     user_message: str = message.content
     channel: str = str(message.channel)
+    # TODO: Log as you go in a log file
     print(f'[{channel:15}] {username:15}: "{user_message}"')
     await send_message(message, user_message)
 
